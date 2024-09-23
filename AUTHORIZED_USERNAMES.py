55489658455817b354db20181f8742d92e22954f
Enzo Allemano <enzo.allemano@hotmail.com>
2024-09-23 08:10:56 +0200

    added le ref

--- AUTHORIZED_USERNAMES.py
+++ AUTHORIZED_USERNAMES.py
@@ -1,2 +1,2 @@
 # This file contain username of authorized Telegram account
-AUTHORIZED_USERNAMES_LIST = ['Enzo_Allemano','Brenda_Taboga']
+AUTHORIZED_USERNAMES_LIST = ['Enzo_Allemano','Brenda_Taboga','dinoallemano']
